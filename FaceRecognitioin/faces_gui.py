db42720ce0aea7c7cfa92ae1cf7c122ebb040f92
Joon_Thinkpad <mjypark1212@gmail.com>
2023-11-13 16:52:51 +0800

    gui v1

--- FaceRecognitioin/faces_gui.py
+++ FaceRecognitioin/faces_gui.py
@@ -8,60 +8,82 @@ from datetime import datetime
 import sys
 import PySimpleGUI as sg
 
-# 1 Create database connection
-myconn = mysql.connector.connect(host="localhost", user="root", passwd="123456", database="facerecognition")
-date = datetime.utcnow()
-now = datetime.now()
-current_time = now.strftime("%H:%M:%S")
-cursor = myconn.cursor()
+
+def feature(result):
+    layout = [
+        [
+            sg.Text(
+                "Class Imminent",
+                size=(18, 1),
+                font=("Any", 18),
+                text_color="#1c86ee",
+                justification="left",
+            )
+        ],
+        [
+            sg.Text("[Class Name]"),
+        ],
+        [
+            sg.Text("[Class Material]"),
+        ],
+        [sg.OK()],
+    ]
+    win = sg.Window(
+        "Attendance System",
+        default_element_size=(21, 1),
+        text_justification="right",
+        auto_size_text=False,
+    ).Layout(layout)
+    event, _ = win.Read(timeout=20)
+    if event is None or event == "Okay":
+        win.Close()
 
 
-#2 Load recognize and read label from model
-recognizer = cv2.face.LBPHFaceRecognizer_create()
-recognizer.read("train.yml")
+def system_start():
+    layout = [
+        [
+            sg.Text(
+                "Setting",
+                size=(18, 1),
+                font=("Any", 18),
+                text_color="#1c86ee",
+                justification="left",
+            )
+        ],
+        [
+            sg.Text("Confidence"),
+            sg.Slider(
+                range=(0, 100),
+                orientation="h",
+                resolution=1,
+                default_value=60,
+                size=(15, 15),
+                key="confidence",
+            ),
+        ],
+        [sg.OK(), sg.Cancel()],
+    ]
+    win = sg.Window(
+        "Attendance System",
+        default_element_size=(21, 1),
+        text_justification="right",
+        auto_size_text=False,
+    ).Layout(layout)
+    event, values = win.Read()
+    if event is None or event == "Cancel":
+        exit()
+    args = values
+    return (args["confidence"], False)
 
-labels = {"person_name": 1}
-with open("labels.pickle", "rb") as f:
-    labels = pickle.load(f)
-    labels = {v: k for k, v in labels.items()}
 
-# create text to speech
-engine = pyttsx3.init()
-rate = engine.getProperty("rate")
-engine.setProperty("rate", 175)
-
-# Define camera and detect face
-face_cascade = cv2.CascadeClassifier('haarcascade/haarcascade_frontalface_default.xml')
-cap = cv2.VideoCapture(0)
-
-
-# 3 Define pysimplegui setting
-layout =  [
-    [sg.Text('Setting', size=(18,1), font=('Any',18),text_color='#1c86ee' ,justification='left')],
-    [sg.Text('Confidence'), sg.Slider(range=(0,100),orientation='h', resolution=1, default_value=60, size=(15,15), key='confidence')],
-    [sg.OK(), sg.Cancel()]
-      ]
-win = sg.Window('Attendance System',
-        default_element_size=(21,1),
-        text_justification='right',
-        auto_size_text=False).Layout(layout)
-event, values = win.Read()
-if event is None or event =='Cancel':
-    exit()
-args = values
-gui_confidence = args["confidence"]
-win_started = False
-
-# 4 Open the camera and start face recognition
-while True:
+def authenticate(cap, face_cascade, recognizer, engine, rate, gui_confidence):
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
 
-    for (x, y, w, h) in faces:
-        print(x, w, y, h)
-        roi_gray = gray[y:y + h, x:x + w]
-        roi_color = frame[y:y + h, x:x + w]
+    for x, y, w, h in faces:
+        roi_gray = gray[y : y + h, x : x + w]
+        roi_color = frame[y : y + h, x : x + w]
         # predict the id and confidence for faces
         id_, conf = recognizer.predict(roi_gray)
 
@@ -80,10 +102,12 @@ while True:
             cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), (2))
 
             # Find the student information in the database.
-            select = "SELECT student_id, name, DAY(login_date), MONTH(login_date), YEAR(login_date) FROM Student WHERE name='%s'" % (name)
+            select = (
+                "SELECT student_id, name, DAY(login_date), MONTH(login_date), YEAR(login_date) FROM Student WHERE name='%s'"
+                % (name)
+            )
             name = cursor.execute(select)
             result = cursor.fetchall()
-            # print(result)
             data = "error"
 
             for x in result:
@@ -101,59 +125,123 @@ while True:
                 Check the course and classroom for the student.
                     If the student has class room within one hour, the corresponding course materials
                         will be presented in the GUI.
-                    if the student does not have class at the moment, the GUI presents a personal class 
+                    if the student does not have class at the moment, the GUI presents a personal class
                         timetable for the student.
 
                 """
-                update =  "UPDATE Student SET login_date=%s WHERE name=%s"
+                update = "UPDATE Student SET login_date=%s WHERE name=%s"
                 val = (date, current_name)
                 cursor.execute(update, val)
                 update = "UPDATE Student SET login_time=%s WHERE name=%s"
                 val = (current_time, current_name)
                 cursor.execute(update, val)
                 myconn.commit()
-               
-                hello = ("Hello ", current_name, "You did attendance today")
+
+                hello = ("Hello ", current_name, " authorized")
                 print(hello)
                 engine.say(hello)
 
+                return (result, frame)
 
         # If the face is unrecognized
-        else: 
+        else:
             color = (255, 0, 0)
             stroke = 2
             font = cv2.QT_FONT_NORMAL
             cv2.putText(frame, "UNKNOWN", (x, y), font, 1, color, stroke, cv2.LINE_AA)
             cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), (2))
-            hello = ("Your face is not recognized")
+            hello = "Your face is not recognized"
             print(hello)
             engine.say(hello)
-            # engine.runAndWait()
+    return (None, frame)
+
+
+# 1 Create database connection
+myconn = mysql.connector.connect(
+    host="localhost", user="root", passwd="admin123", database="facerecognition"
+)
+date = datetime.utcnow()
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
+cursor = myconn.cursor()
+
+
+# 2 Load recognize and read label from model
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read("train.yml")
+
+labels = {"person_name": 1}
+with open("labels.pickle", "rb") as f:
+    labels = pickle.load(f)
+    labels = {v: k for k, v in labels.items()}
+
+# create text to speech
+engine = pyttsx3.init()
+rate = engine.getProperty("rate")
+engine.setProperty("rate", 175)
+
+# Define camera and detect face
+face_cascade = cv2.CascadeClassifier("haarcascade/haarcascade_frontalface_default.xml")
+cap = cv2.VideoCapture(0)
+
+
+# 3 Define pysimplegui setting
+gui_confidence, win_started = system_start()
+
+# 4 Open the camera and start face recognition
+while True:
+    student_data, frame = authenticate(
+        cap=cap,
+        face_cascade=face_cascade,
+        recognizer=recognizer,
+        engine=engine,
+        rate=rate,
+        gui_confidence=gui_confidence,
+    )
+
+    if student_data:
+        feature(student_data)
 
     # GUI
-    imgbytes = cv2.imencode('.png', frame)[1].tobytes() 
+    imgbytes = cv2.imencode(".png", frame)[1].tobytes()
     if not win_started:
         win_started = True
         layout = [
-            [sg.Text('Attendance System Interface', size=(30,1))],
-            [sg.Image(data=imgbytes, key='_IMAGE_')],
-            [sg.Text('Confidence'),
-                sg.Slider(range=(0, 100), orientation='h', resolution=1, default_value=60, size=(15, 15), key='confidence')],
-            [sg.Exit()]
+            [sg.Text("Attendance System Interface", size=(30, 1))],
+            [sg.Image(data=imgbytes, key="_IMAGE_")],
+            [
+                sg.Text("Confidence"),
+                sg.Slider(
+                    range=(0, 100),
+                    orientation="h",
+                    resolution=1,
+                    default_value=60,
+                    size=(15, 15),
+                    key="confidence",
+                ),
+            ],
+            [sg.Exit()],
         ]
-        win = sg.Window('Attendance System',
+        win = (
+            sg.Window(
+                "Attendance System",
                 default_element_size=(14, 1),
-                text_justification='right',
-                auto_size_text=False).Layout(layout).Finalize()
-        image_elem = win.FindElement('_IMAGE_')
+                text_justification="right",
+                auto_size_text=False,
+            )
+            .Layout(layout)
+            .Finalize()
+        )
+        image_elem = win.FindElement("_IMAGE_")
     else:
         image_elem.Update(data=imgbytes)
 
     event, values = win.Read(timeout=20)
-    if event is None or event == 'Exit':
+    if event is None or event == "Exit":
+        win.close()
         break
-    gui_confidence = values['confidence']
+    gui_confidence = values["confidence"]
 
-        
+
 win.Close()
 cap.release()
